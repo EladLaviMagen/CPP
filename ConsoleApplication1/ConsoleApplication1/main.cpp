481cde064f5bc5519d7ac8bb2c7c2dfd0fb64462
EladLaviMagen <Eladmagen10@gmail.com>
2025-06-17 12:14:29 +0300

    PR fixes

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,31 +1,11 @@
-#include <cctype>
 #include <iostream>
-#include <string>
 
-bool isNumber(std::string str);
 
 int main() {
-    std::string number = "";
+    int number = -1;
     std::cout << "Please enter your number : ";
-    std::getline(std::cin, number);
-    if (!isNumber(number)) {
-        std::cout << "INVALID INPUT" << std::endl;
-        return 1;
-    }
-    std::cout << "G-Root : " << std::sqrt(std::stoi(number));
+    std::cin >> number;
+    std::cout << "G-Root : " << std::sqrt(number);
     return 0;
 }
 
-/*
- * Checks if a string only has digits
- * :Param str: [IN] The string to check
- * :Return: True if string represents a number, false otherwise
- */
-bool isNumber(std::string str) {
-    for (unsigned int i = 0; i < str.length(); i++) {
-        if (!std::isdigit(str[i])) {
-            return false;
-        }
-    }
-    return true;
-}
