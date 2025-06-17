fe561a4ba4b5571f0e37694bca8e0bf347177d38
EladLaviMagen <Eladmagen10@gmail.com>
2025-06-17 12:09:20 +0300

    Added initial code

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+std::string pad(int number);
+
+int main() {
+    for (int i = 1; i <= 11; i++) {
+        for (int j = 1; j <= 11; j++) {
+            std::cout << i * j << pad(i * j);
+        }
+        std::cout << std::endl;
+    }
+}
+
+std::string pad(int number) {
+    std::string spaces = "";
+    while (number != 0) {
+        number /= 10;
+        spaces[spaces.length() - 1] = '\0';
+    }
+    return spaces;
 }
