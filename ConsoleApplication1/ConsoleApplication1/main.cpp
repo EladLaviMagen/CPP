583d204a13d7cda655a07f41242bd9f4112ebb72
EladLaviMagen <Eladmagen10@gmail.com>
2025-06-17 12:25:13 +0300

    Negative number handling

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -5,6 +5,10 @@ int main() {
     int number = -1;
     std::cout << "Please enter your number : ";
     std::cin >> number;
+    if (number < 0) {
+        std::cout << "INVALID INPUT";
+        return 1;
+    }
     std::cout << "G-Root : " << std::sqrt(number);
     return 0;
 }
