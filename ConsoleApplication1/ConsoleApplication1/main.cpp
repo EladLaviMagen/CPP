0ff1db43204e03470689167602b4352ab3ee54bb
EladLaviMagen <Eladmagen10@gmail.com>
2025-06-17 16:04:22 +0300

    Better const name

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -3,12 +3,12 @@
 
 #define SPACE " "
 #define MAX_PAD 5
-#define ELEVEN 11
+#define BOARD_SIZE 11
 
 int main() {
-    for (int i = 1; i <= ELEVEN; i++) {
-        for (int j = 1; j <= ELEVEN; j++) {
-            std::cout << std::setw(5) << i * j;
+    for (int i = 1; i <= BOARD_SIZE; i++) {
+        for (int j = 1; j <= BOARD_SIZE; j++) {
+            std::cout << std::setw(MAX_PAD) << i * j;
         }
         std::cout << std::endl;
     }
