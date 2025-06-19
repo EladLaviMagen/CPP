dd955da6a9bcd895c484647968bc7118a42ce074
EladLaviMagen <Eladmagen10@gmail.com>
2025-06-19 14:35:19 +0300

    First version

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -18,7 +18,8 @@ std::string spacePrint(std::string a, int b) {
     return std::move(a) + std::to_string(b) + ' ';
 }
 
-int subtruct(int num) {
+
+int subtructFromPrev(int num) {
     static int prev;
     int res = prev - num;
     prev = num;
@@ -31,11 +32,11 @@ int main()
     std::cout << std::count(vec.begin(), vec.end(), 4) << std::endl;
     std::cout << std::count_if(vec.begin(), vec.end(), largerThanFour) << std::endl;
     vec.erase(std::remove_if(vec.begin(), vec.end(), lessThanEight), vec.end());
-    std::cout << std::accumulate(vec.begin(), vec.end(), std::string(), spacePrint);
+    std::cout << std::accumulate(vec.begin(), vec.end(), std::string(), spacePrint) << std::endl;
     std::vector<int> vec2;
-    std::accumulate(vec.begin(), vec.end(), vec2, subtruct);
-    
-
-
+    ///Regarding section 7 because the first element does not have an element before it, I treat it like there is a 0 prior to it.
+    std::transform(vec.cbegin(), vec.cend(), std::back_inserter(vec2), subtructFromPrev);
+    std::cout << std::accumulate(vec2.begin(), vec2.end(), std::string(), spacePrint) << std::endl;
+    std::cout << std::accumulate(vec2.begin(), vec2.end(), 0) << std::endl;
     return 0;
 }
